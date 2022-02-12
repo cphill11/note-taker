b97223f0b67e3677e80094e8ae39d7fdbe0097d7
Criste Phillips <criste.phillips@yahoo.com>
2022-02-12 13:49:28 -0500

    excess code removed

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,13 @@
 const router = require("express").Router();
 const notes = require("../../lib/notes");
 const getNewNotes = require("../../lib/notes");
-// const { notes } = require("../../db/db.json");
 
-
-// get (path problem here???)   // public/assets/js/index.js
 router.get("/", (req, res) => {
  getNewNotes.read().then((notes) => {
     return res.json(notes);
   });
 });
 
-// post  (path problem here???)
 router.post("/", (req, res) => {
   getNewNotes.write(req.body).then((notes)=> {
     res.json(notes);
@@ -19,8 +15,6 @@ router.post("/", (req, res) => {
   .catch((error) => res.status(500).json(error));
 });
 
-
-// delete not right; add colon??
 router.delete('/:id', (req, res) => {
   getNewNotes.deleteNotes(req.params.id).then(() => {
     res.json({ok:true});
@@ -29,7 +23,6 @@ router.delete('/:id', (req, res) => {
     console.log(error);
     res.status(500).json(error)
   })
-   //res.json(req.body);
 });
 
 module.exports = router;
